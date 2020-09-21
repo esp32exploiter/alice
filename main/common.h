015ad3b2e144a8d0384a3ea30803fd7c6ac7558b
Xing GUO <higuoxing@gmail.com>
2020-09-21 14:48:19 +0800

    First commit.

---
+++ main/common.h
@@ -0,0 +1,15 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "esp_err.h"
+
+void init_fucking_nvs_flash();
+void init_fucking_net_interface();
+void init_fucking_wifi();
+void start_fucking_wifi();
+
+uint64_t create_fucking_beacon_package(uint8_t beacon_rick[200],
+                                       const char* ssid,
+                                       uint8_t id);
+uint64_t create_fucking_data_packet(uint8_t data_rick[200], uint32_t id);
+#endif
